35120b04ff5635eeb252bcf38f1ad1171fa80c17
abi2310 <abilash.shanmugavel23@outlook.de>
2025-01-16 15:55:07 +0100

    Feature[BA]: Added Github Action to fetch Pull Request Data

---
+++ .github/OpenAi/OpenAi_File.py
@@ -0,0 +1,54 @@
+import argparse
+from openai import OpenAI
+
+parser = argparse.ArgumentParser(description="Send input to OpenAI API and get a concise changelog.")
+parser.add_argument("api_key", help="Your OpenAI API key.", type=str)
+parser.add_argument('filename')
+parser.add_argument("prompt", help="Prompt to customize the OpenAI API behavior.", type=str)
+args = parser.parse_args()
+
+client = OpenAI(api_key=args.api_key)
+
+assistant = client.beta.assistants.create(
+    name="Developer",
+    instructions="You are a Software Developer with high knowlinge in software security",
+    tools=[{"type": "file_search"}],
+    model="gpt-4o-mini",
+)
+
+# Funktion, um eine Datei hochzuladen und an eine Anfrage anzuhängen
+def process_file_and_query(file_path, user_query):
+    # Datei hochladen
+    file_stream = open(file_path, "rb")
+    message_file = client.files.create(file=file_stream, purpose="assistants")
+
+    # Thread erstellen und Datei anhängen
+    thread = client.beta.threads.create(
+        messages=[
+            {
+                "role": "user",
+                "content": user_query,
+                "attachments": [{"file_id": message_file.id, "tools": [{"type": "file_search"}]}],
+            }
+        ]
+    )
+
+    # Run erstellen und auf Antwort warten
+    run = client.beta.threads.runs.create_and_poll(thread_id=thread.id, assistant_id=assistant.id)
+    messages = list(client.beta.threads.messages.list(thread_id=thread.id, run_id=run.id))
+    if not messages:
+        raise ValueError("No messages received from the OpenAI API.")
+    if not messages[0].content:
+        raise ValueError("No content in the first message.")
+    # Antwort abrufen
+    response = messages[0].content[0].text
+
+    # Datei löschen, um Speicher zu schonen
+    client.files.delete(file_id=message_file.id)
+
+    return response
+
+# Beispielnutzung
+
+response = process_file_and_query(args.filename, args.prompt)
+print(response)
